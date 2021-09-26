cad67541c1feef6cba454af984b7f98edd8120c4
Josh Lyon <joshlyon@udel.edu>
2021-09-26 14:42:34 -0400

    Add course website to navigation bar

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ function navToUrl(url) {
 const navHtml = `<div class="header-footer">
 <ul class="nav-bar">
     <li class="nav-bar">
-        <button class="nav-btn" onclick="navToUrl(\'index.html\')">Home</button>
-        <button class="nav-btn" onclick="navToUrl(\'aboutMe.html')">About Me</button>
+        <button class="nav-btn" onclick="navToUrl('index.html')">Home</button>
+        <button class="nav-btn" onclick="navToUrl('aboutMe.html')">About Me</button>
+        <button class="nav-btn" onclick="navToUrl('https://mattm401.github.io/')">Course Website</button>
         <button class="nav-btn" onclick="navToUrl('https://github.com/joshualyon7')">My Github</button>
         <button class="nav-btn" onclick="navToUrl('https://github.com/joshualyon7/joshualyon7.github.io')">Website Source</button>
         <button class="nav-btn" onclick="navToUrl('https://aaronliu20.github.io/')">Previous Student</button>
